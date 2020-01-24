955be7fc2153859a1126bbe94eff6801727cc583
nahidaCoU39 <50311828+nahidaCoU39@users.noreply.github.com>
2020-01-23 23:12:19 -0800

    Add files via upload

---
+++ rearrange/homepage.php
@@ -0,0 +1,174 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home page</title>
+
+    <link rel="stylesheet" type="text/css" href="decorate.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+</head>
+<body>
+
+	<div class="container">
+
+		<!---nevigation bar------------->
+
+		<div class="navbar">
+          <a href="#home">Home</a>
+          
+          <div class="jobs">
+             <button class="jobsbt">JOBS
+             <i class="fa fa-caret-down"></i>
+             </button>
+              <div class="jobs-content">
+                <a href="bcs/BCS_info.html">BCS</a>
+                <a href="bank/m.html">BANK</a>
+                <a href="psc/index.html">PSC</a>
+              </div>
+          </div>
+
+          <div class="question">
+             <button class="questionbt">QUESTION BANK
+             <i class="fa fa-caret-down"></i>
+             </button>
+              <div class="question-content">
+                <a href="https://www.bcsstudy.com/bcs-question-solution-pdf-download/">BCS QUESTION</a>
+                <a href="bank/page 4.html">BANK QUESTION</a>
+                <a href="https://jobstestbd.com/bpsc-non-cadre-exam-question-solution-2019/"">PSC QUESTION</a>
+              </div>
+          </div>
+
+          <a href="#news">ABOUT US</a>
+        </div>
+
+        <!-------banner-------->
+
+        <div class="banner">
+          <img class="mySlides" src="images/2nd.jpg" alt="custom_html_banner1" style="width:100%">
+
+        </div>
+
+        <!--------------search bar------->
+        <div class="search">
+        	<form class="example" >
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+
+        </div>
+
+
+
+        <!---------------                    -------------------->
+
+        <div class="description">
+        	
+        		  <div class="row">
+                <div class="column">
+                  <div class="card1">
+                    <h2>All Bank Job </h2>
+                    <div class="fakeimg" ><img class="mySlides" src="images/bank.png" alt="" style="width:100%"></div>
+                  
+                      
+
+                        <p>All Government Bank Job Preparation in Bangladesh.Large collection of previous year questions,exam syllabus,exam procedure,and others helpfull informations..           </p>
+
+                       </div> 
+              </div>
+
+        		  
+
+
+                <div class="column">
+                  <div class="card2">
+                    <h2>All BPSC JOBS </h2>
+                    <div class="fakeimg" ><img class="mySlides" src="images/bpsc-job.jpg" alt="" style="width:100%"></div>
+                  
+                      
+
+                        <p>All Bangladesh Public Service Comission Job Preparation in Bangladesh.Large collection of previous year questions,exam syllabus,exam procedure,and others helpfull informations..           </p>
+
+                       </div> 
+              </div>
+
+
+              <div class="column">
+                  <div class="card3">
+                    <h2>BCS Jobs </h2>
+                    <div class="fakeimg" ><img class="mySlides" src="images/bcs.jpg" alt="" style="width:100%"></div>
+                  
+                      
+
+                        <p>All BCS Job Preparation in Bangladesh.Large collection of previous year questions,exam syllabus,exam procedure,and others helpfull informations..           </p>
+
+                       </div> 
+              </div>
+
+
+
+              
+
+
+              </div>
+
+                
+    
+        </div>
+
+
+
+
+        <!-------------footer----------------->
+
+        <div class="foot">
+        	<div class="row1">
+        		<div class="para">
+        			<p>© 2019</p>
+        			
+        		</div>
+
+        		<div class="c1">
+        			<p>Question Banks</p>
+        		</div>
+
+        		<div class="c1">
+        			<p>Jobs</p>
+        		</div>
+
+        		<div class="c1">
+        			<p>Tips</p>
+        		</div>
+           
+
+           <div class="icon-bar">
+  <a class="active" href="#"><i class="fa fa-facebook"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-whatsapp"></i></a>
+  <a href="#"><i class=""></i></a>
+  <a href="#"><i class=""></i></a>
+</div>
+        		
+        	</div>
+        	
+        </div>
+        		
+        	
+        	
+        
+
+
+
+
+
+
+
+
+		
+	</div>
+
+</body>
+</html>
